413ace4da86b908962e85ae914d8d789bbb672be
Speechless22 <61347609+Speechless22@users.noreply.github.com>
2023-04-01 21:47:26 +0800

    重构

---
+++ diy-config.sh
@@ -0,0 +1,11 @@
+echo '# CONFIG_PACKAGE_dnsmasq is not set' >> .config
+echo 'CONFIG_LUCI_LANG_zh_Hans=y' >> .config
+echo 'CONFIG_PACKAGE_docker-compose=y' >> .config
+echo 'CONFIG_PACKAGE_dockerd=y' >> .config
+echo 'CONFIG_PACKAGE_git-http=y' >> .config
+echo 'CONFIG_PACKAGE_wget-ssl=y' >> .config
+echo 'CONFIG_PACKAGE_luci-app-aria2=y' >> .config
+echo 'CONFIG_PACKAGE_luci-app-diskman=y' >> .config
+echo 'CONFIG_PACKAGE_luci-app-dockerman=y' >> .config
+echo 'CONFIG_PACKAGE_luci-app-openclash=y' >> .config
+echo 'CONFIG_PACKAGE_luci-app-samba4=y' >> .config
