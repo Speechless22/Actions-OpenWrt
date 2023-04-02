fd24268fad95b833f31f91736de851452bb1e1ce
Speechless22 <61347609+Speechless22@users.noreply.github.com>
2023-04-02 12:30:28 +0800

    ADD

--- diy-config.sh
+++ diy-config.sh
@@ -8,5 +8,5 @@ echo 'CONFIG_PACKAGE_luci-app-aria2=y' >> .config
 echo 'CONFIG_PACKAGE_luci-app-dockerman=y' >> .config
 echo 'CONFIG_PACKAGE_luci-app-openclash=y' >> .config
 echo 'CONFIG_PACKAGE_luci-app-samba4=y' >> .config
-#echo 'CONFIG_PACKAGE_luci-app-diskman=y' >> .config
+echo 'CONFIG_PACKAGE_luci-app-diskman=y' >> .config
 echo 'CONFIG_TARGET_ROOTFS_PARTSIZE=1024' >> .config
