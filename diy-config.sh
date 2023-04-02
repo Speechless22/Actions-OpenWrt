7fb4faf5ef80d126f2302b4f78e6bb9d127d4f5b
Speechless22 <61347609+Speechless22@users.noreply.github.com>
2023-04-02 19:11:01 +0800

    以默认配置测试

--- diy-config.sh
+++ diy-config.sh
@@ -1,12 +1,12 @@
-echo '# CONFIG_PACKAGE_dnsmasq is not set' >> .config
-echo 'CONFIG_LUCI_LANG_zh_Hans=y' >> .config
-echo 'CONFIG_PACKAGE_docker-compose=y' >> .config
-echo 'CONFIG_PACKAGE_dockerd=y' >> .config
-echo 'CONFIG_PACKAGE_git-http=y' >> .config
-echo 'CONFIG_PACKAGE_wget-ssl=y' >> .config
-echo 'CONFIG_PACKAGE_luci-app-aria2=y' >> .config
-echo 'CONFIG_PACKAGE_luci-app-dockerman=y' >> .config
-echo 'CONFIG_PACKAGE_luci-app-openclash=y' >> .config
-echo 'CONFIG_PACKAGE_luci-app-samba4=y' >> .config
-echo 'CONFIG_PACKAGE_luci-app-diskman=y' >> .config
-echo 'CONFIG_TARGET_ROOTFS_PARTSIZE=1024' >> .config
+#echo '# CONFIG_PACKAGE_dnsmasq is not set' >> .config
+#echo 'CONFIG_LUCI_LANG_zh_Hans=y' >> .config
+#echo 'CONFIG_PACKAGE_docker-compose=y' >> .config
+#echo 'CONFIG_PACKAGE_dockerd=y' >> .config
+#echo 'CONFIG_PACKAGE_git-http=y' >> .config
+#echo 'CONFIG_PACKAGE_wget-ssl=y' >> .config
+#echo 'CONFIG_PACKAGE_luci-app-aria2=y' >> .config
+#echo 'CONFIG_PACKAGE_luci-app-dockerman=y' >> .config
+#echo 'CONFIG_PACKAGE_luci-app-openclash=y' >> .config
+#echo 'CONFIG_PACKAGE_luci-app-samba4=y' >> .config
+#echo 'CONFIG_PACKAGE_luci-app-diskman=y' >> .config
+#echo 'CONFIG_TARGET_ROOTFS_PARTSIZE=1024' >> .config
--- diy-packages.sh
+++ diy-packages.sh
@@ -1,8 +1,8 @@
-# 添加luci-app-openclash
-git clone --depth 1 --branch master https://github.com/vernesong/OpenClash.git
-mv OpenClash/luci-app-openclash openwrt/package
-# 添加luci-app-diskman
-mkdir -p openwrt/package/luci-app-diskman && \
-wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/applications/luci-app-diskman/Makefile -O openwrt/package/luci-app-diskman/Makefile
-mkdir -p openwrt/package/parted && \
-wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile -O openwrt/package/parted/Makefile
+## 添加luci-app-openclash
+#git clone --depth 1 --branch master https://github.com/vernesong/OpenClash.git
+#mv OpenClash/luci-app-openclash openwrt/package
+## 添加luci-app-diskman
+#mkdir -p openwrt/package/luci-app-diskman && \
+#wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/applications/luci-app-diskman/Makefile -O openwrt/package/luci-app-diskman/Makefile
+#mkdir -p openwrt/package/parted && \
+#wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile -O openwrt/package/parted/Makefile
