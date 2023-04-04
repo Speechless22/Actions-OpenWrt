4dda2fc18e9c6d38237af24d163448ab460d796a
Speechless22 <61347609+Speechless22@users.noreply.github.com>
2023-04-04 20:28:49 +0800

    fix

--- custom-files.sh
+++ custom-files.sh
@@ -1,15 +1,15 @@
-mkdir -p $BUILDROOT/files/etc/openclash/core
+mkdir -p $GITHUB_WORKSPACE/openwrt/files/etc/openclash/core
 
 # 为luci-app-openclash添加内核
 git clone --depth 1 --branch core https://github.com/vernesong/OpenClash.git OpenClash-core
 rm OpenClash-core/*/*/clash-linux-amd64-v3*
-tar xOvz OpenClash-core/master/dev/clash-linux-amd64.tar.gz > $BUILDROOT/files/etc/openclash/core/clash
-gunzip -c OpenClash-core/master/premium/clash-linux-amd64*.gz > $BUILDROOT/files/etc/openclash/core/clash_tun
-tar xOvz OpenClash-core/master/meta/clash-linux-amd64.tar.gz > $BUILDROOT/files/etc/openclash/core/clash_meta
-chmod +x $BUILDROOT/files/etc/openclash/core/clash*
+tar xOvz OpenClash-core/master/dev/clash-linux-amd64.tar.gz > $GITHUB_WORKSPACE/openwrt/files/etc/openclash/core/clash
+gunzip -c OpenClash-core/master/premium/clash-linux-amd64*.gz > $GITHUB_WORKSPACE/openwrt/files/etc/openclash/core/clash_tun
+tar xOvz OpenClash-core/master/meta/clash-linux-amd64.tar.gz > $GITHUB_WORKSPACE/openwrt/files/etc/openclash/core/clash_meta
+chmod +x $GITHUB_WORKSPACE/openwrt/files/etc/openclash/core/clash*
 
 # 为luci-app-openclash添加外部数据
 GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
 GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
-wget -qO- $GEOIP_URL > $BUILDROOT/files/etc/openclash/GeoIP.dat
-wget -qO- $GEOSITE_URL > $BUILDROOT/files/etc/openclash/GeoSite.dat
+wget -qO- $GEOIP_URL > $GITHUB_WORKSPACE/openwrt/files/etc/openclash/GeoIP.dat
+wget -qO- $GEOSITE_URL > $GITHUB_WORKSPACE/openwrt/files/etc/openclash/GeoSite.dat
