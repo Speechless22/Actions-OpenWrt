f142c8fb35b2337f1dbe98d46bbe217bcfdf88bc
ライザ <61347609+Speechless22@users.noreply.github.com>
2024-02-12 10:31:05 +0800

    更新 custom-packages.sh

--- custom-packages.sh
+++ custom-packages.sh
@@ -1,4 +1,4 @@
-# 加载luci-app-openclash
+# Load luci-app-openclash
 
 git clone --depth 1 --branch master https://github.com/vernesong/OpenClash.git
 mv OpenClash/luci-app-openclash $BUILDROOT/package
