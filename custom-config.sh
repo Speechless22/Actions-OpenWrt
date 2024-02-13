3e23a1f40aaf042bf9eb96a82f5f6ec783193744
ライザ <61347609+Speechless22@users.noreply.github.com>
2024-02-13 13:01:14 +0800

    update

--- custom-config.sh
+++ custom-config.sh
@@ -1,26 +1,26 @@
-# 利用make defconfig可生成完整.config文件
+# Use "make defconfig" to generate a complete .config file
 echo 'CONFIG_TARGET_x86=y' >> $BUILDROOT/.config
 echo 'CONFIG_TARGET_x86_64=y' >> $BUILDROOT/.config
 echo 'CONFIG_TARGET_x86_64_DEVICE_generic=y' >> $BUILDROOT/.config
 
-# 自定义固件基础
+# Custom firmware base
 echo 'CONFIG_LUCI_LANG_zh_Hans=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_luci=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_luci-ssl=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_git-http=y' >> $BUILDROOT/.config
 echo 'CONFIG_TARGET_ROOTFS_PARTSIZE=3072' >> $BUILDROOT/.config
 
-# 添加luci-app-openclash
+# Add luci-app-openclash
 echo 'CONFIG_PACKAGE_luci-app-openclash=y' >> $BUILDROOT/.config
 echo '# CONFIG_PACKAGE_dnsmasq is not set' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_luci-compat=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_wget-ssl=y' >> $BUILDROOT/.config
 
-# 添加luci-app-dockerman
+# Add luci-app-dockerman
 echo 'CONFIG_PACKAGE_luci-app-dockerman=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_dockerd=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_docker-compose=y' >> $BUILDROOT/.config
 
-# 添加其他app
+# Add other apps
 echo 'CONFIG_PACKAGE_luci-app-aria2=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_luci-app-samba4=y' >> $BUILDROOT/.config
