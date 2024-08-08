88e48307b6b0e23c0ecfe753be5e0b36e228375a
ライザ <61347609+Speechless22@users.noreply.github.com>
2024-08-08 23:06:21 +0800

    re

--- custom-config.sh
+++ custom-config.sh
@@ -9,7 +9,6 @@
 echo 'CONFIG_TARGET_mediatek=y' >> $BUILDROOT/.config
 echo 'CONFIG_TARGET_mediatek_filogic=y' >> $BUILDROOT/.config
 echo 'CONFIG_TARGET_mediatek_filogic_DEVICE_bananapi_bpi-r3-mini=y' >> $BUILDROOT/.config
-echo 'CONFIG_TARGET_ROOTFS_PARTSIZE=7168' >> $BUILDROOT/.config
 
 # Lanuage Setting
 echo 'CONFIG_LUCI_LANG_zh_Hans=y' >> $BUILDROOT/.config
@@ -28,6 +27,6 @@ echo 'CONFIG_PACKAGE_docker-compose=y' >> $BUILDROOT/.config
 # Add other apps
 echo 'CONFIG_PACKAGE_luci=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_luci-ssl=y' >> $BUILDROOT/.config
-echo 'CONFIG_PACKAGE_git-http=y' >> $BUILDROOT/.config
-echo 'CONFIG_PACKAGE_luci-app-aria2=y' >> $BUILDROOT/.config
-echo 'CONFIG_PACKAGE_luci-app-samba4=y' >> $BUILDROOT/.config
+# echo 'CONFIG_PACKAGE_git-http=y' >> $BUILDROOT/.config
+# echo 'CONFIG_PACKAGE_luci-app-aria2=y' >> $BUILDROOT/.config
+# echo 'CONFIG_PACKAGE_luci-app-samba4=y' >> $BUILDROOT/.config
