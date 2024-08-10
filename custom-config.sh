f730c6b749d8d2e64f12647d7b417fbc7b5ba477
ライザ <61347609+Speechless22@users.noreply.github.com>
2024-08-10 10:14:43 +0800

    re

--- custom-config.sh
+++ custom-config.sh
@@ -20,13 +20,12 @@ echo 'CONFIG_PACKAGE_luci-compat=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_wget-ssl=y' >> $BUILDROOT/.config
 
 # Add Docker
-# echo 'CONFIG_PACKAGE_luci-app-dockerman=y' >> $BUILDROOT/.config
-# echo 'CONFIG_PACKAGE_dockerd=y' >> $BUILDROOT/.config
-# echo 'CONFIG_PACKAGE_docker-compose=y' >> $BUILDROOT/.config
+echo 'CONFIG_PACKAGE_luci-app-dockerman=y' >> $BUILDROOT/.config
+echo 'CONFIG_PACKAGE_dockerd=y' >> $BUILDROOT/.config
+echo 'CONFIG_PACKAGE_docker-compose=y' >> $BUILDROOT/.config
 
 # Add other apps
 echo 'CONFIG_PACKAGE_luci=y' >> $BUILDROOT/.config
 echo 'CONFIG_PACKAGE_luci-ssl=y' >> $BUILDROOT/.config
-# echo 'CONFIG_PACKAGE_git-http=y' >> $BUILDROOT/.config
-# echo 'CONFIG_PACKAGE_luci-app-aria2=y' >> $BUILDROOT/.config
-# echo 'CONFIG_PACKAGE_luci-app-samba4=y' >> $BUILDROOT/.config
+echo 'CONFIG_PACKAGE_luci-app-aria2=y' >> $BUILDROOT/.config
+echo 'CONFIG_PACKAGE_luci-app-samba4=y' >> $BUILDROOT/.config
