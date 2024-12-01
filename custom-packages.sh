207221dc4b75a4592849211f1285a1d40ad5ec95
ライザ <61347609+Speechless22@users.noreply.github.com>
2024-12-02 00:04:09 +0800

    fix

--- custom-packages.sh
+++ custom-packages.sh
@@ -5,6 +5,6 @@ mv OpenClash/luci-app-openclash $BUILD_ROOT/package
 
 # Load luci-app-diskman
 mkdir -p $BUILD_ROOT/package/luci-app-diskman && \
-wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/applications/luci-app-diskman/Makefile -O $BUILD_ROOT/package/luci-app-diskman/Makefile
+wget https://raw.githubusercontent.com/Speechless22/luci-app-diskman/master/applications/luci-app-diskman/Makefile -O $BUILD_ROOT/package/luci-app-diskman/Makefile
 mkdir -p $BUILD_ROOT/package/parted && \
-wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile -O $BUILD_ROOT/package/parted/Makefile
+wget https://raw.githubusercontent.com/Speechless22/luci-app-diskman/master/Parted.Makefile -O $BUILD_ROOT/package/parted/Makefile
