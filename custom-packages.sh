c7f8a8f4ec5fda144596a11a489601d5e9900035
ライザ <61347609+Speechless22@users.noreply.github.com>
2024-12-02 08:32:00 +0800

    Update custom-packages.sh

--- custom-packages.sh
+++ custom-packages.sh
@@ -1,6 +1,6 @@
 # Load luci-app-openclash
 
-git clone --depth 1 --branch master https://github.com/vernesong/OpenClash.git
+git clone --depth 1 --branch master https://github.com/Speechless22/OpenClash.git
 mv OpenClash/luci-app-openclash $BUILD_ROOT/package
 
 # Load luci-app-diskman
