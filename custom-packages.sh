479aa9503027e65769fe0ace58db52f2050fa11f
ライザ <61347609+Speechless22@users.noreply.github.com>
2024-12-04 21:56:20 +0800

    opt: use official Disk Support

--- custom-packages.sh
+++ custom-packages.sh
@@ -2,12 +2,3 @@
 
 git clone --depth 1 --branch master https://github.com/Speechless22/OpenClash.git
 mv OpenClash/luci-app-openclash $BUILD_ROOT/package
-
-# Load luci-app-diskman
-# mkdir -p $BUILD_ROOT/package/luci-app-diskman && \
-# wget https://raw.githubusercontent.com/Speechless22/luci-app-diskman/master/applications/luci-app-diskman/Makefile -O $BUILD_ROOT/package/luci-app-diskman/Makefile
-# mkdir -p $BUILD_ROOT/package/parted && \
-# wget https://raw.githubusercontent.com/Speechless22/luci-app-diskman/master/Parted.Makefile -O $BUILD_ROOT/package/parted/Makefile
-
-git clone --depth 1 --branch master https://github.com/Speechless22/luci-app-diskman.git
-mv luci-app-diskman/applications/luci-app-diskman $BUILD_ROOT/package
