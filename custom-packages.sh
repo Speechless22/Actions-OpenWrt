aee97f7e24c52be061a5e21209355a05bcf75b15
Speechless22 <61347609+Speechless22@users.noreply.github.com>
2025-01-01 17:32:38 +0800

    re

--- custom-packages.sh
+++ custom-packages.sh
@@ -1,4 +1,4 @@
 # Load luci-app-openclash
 
-git clone --depth 1 --branch master https://github.com/Speechless22/OpenClash.git
+git clone --depth 1 --branch master https://github.com/vernesong/OpenClash.git
 mv OpenClash/luci-app-openclash $BUILD_ROOT/package
